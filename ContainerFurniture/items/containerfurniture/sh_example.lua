302bed954aa9e1073a3603889443f3c56393a918
Akiran7219 <56609801+Akiran7219@users.noreply.github.com>
2021-06-26 18:58:24 +0200

    Update sh_example.lua

--- ContainerFurniture/items/containerfurniture/sh_example.lua
+++ ContainerFurniture/items/containerfurniture/sh_example.lua
@@ -1,11 +1,4 @@
 ITEM.name = "Example Container Furniture"
 ITEM.description = "Example description"
--- Inventory Model
 ITEM.model = "models/Items/item_item_crate.mdl"
-
---[[Container model
-
- MAKE SURE YOU HAVE REGISTER A CONTAINER IN HELIX/PLUGINS/CONTAINERS/sh_definitions.lua
-
---]]
 ITEM.ContainerModel = "models/props_c17/lockers001a.mdl"
