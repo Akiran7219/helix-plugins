9c310b4e9c50ab071e13df256f94b5e646b9237e
Akiran7219 <56609801+Akiran7219@users.noreply.github.com>
2021-07-01 03:55:55 +0200

    Port from Clockwork Union Light

---
+++ UnionLight/entities/entities/cw_unionlight/shared.lua
@@ -0,0 +1,8 @@
+DEFINE_BASECLASS("base_gmodentity");
+
+ENT.Type = "anim";
+ENT.Author = "RJ";
+ENT.Category = "Redemption";
+ENT.PrintName = "Union Light";
+ENT.Spawnable = true;
+ENT.AdminSpawnable = true;
---
+++ UnionLight/sh_plugin.lua
@@ -0,0 +1,3 @@
+PLUGIN.name = "Union Light"
+PLUGIN.description  = "Adds a union light to illuminate an area."
+PLUGIN.author = "RJ(Clockwork), Akiran (Helix Port)"
